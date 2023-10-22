16c6971d47fa5fc5e65f763ce09c17fcbf2f0378
iMac <imac@iMacs-iMac.local>
2023-10-22 20:08:45 +0900

    update local

--- src/server.ts
+++ src/server.ts
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 app.use(router);
 
-server.listen(PORT, HOSTNAME, () => {
+server.listen(PORT, () => {
     console.warn(`server runing on port ${PORT}`);
 });
 
