e5c8281d8424a9fd01701141a84aca4e779bc4af
iMac <imac@iMacs-iMac.local>
2023-11-20 23:04:57 +0900

    ok

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express, { Application, application, Request, Response } from 'express';
+import express, { Application } from 'express';
 import cors from 'cors';
 import router from './routers';
 import dotenv from 'dotenv';
