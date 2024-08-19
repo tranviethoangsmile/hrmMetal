507e6d0d8969e665b36e74f10e6ec74e47e19e1a
hoangdev <hoangdev@hoangdevs-iMac.local>
2024-08-19 11:50:59 +0900

    update

--- src/useCases/product/product.useCase.ts
+++ src/useCases/product/product.useCase.ts
@@ -10,39 +10,27 @@ import {
     valid_create_product,
     valid_search_product,
 } from '../../validates/product/product.validate';
-import { Products } from '../../enum/product.enum';
+import { Products } from '../../enum';
 const product_create = async (data: product_field_create) => {
     try {
-        const valid = await valid_create_product(data);
-        if (!valid.error) {
-            if (
-                typeof data.name === 'string' &&
-                Object.values(Products).includes(data.name)
-            ) {
-                const product = await create_product(data);
-                if (product?.success) {
-                    return {
-                        success: true,
-                        data: product?.data,
-                    };
-                } else {
-                    return {
-                        success: false,
-                        message: product?.message,
-                    };
-                }
-            } else {
-                return {
-                    success: false,
-                    message: 'product name not avaliable',
-                };
-            }
-        } else {
-            return {
-                success: false,
-                message: valid.error.message,
-            };
+        const valid = valid_create_product(data);
+        if (valid.error) {
+            throw new Error(`${valid?.error.message}`);
         }
+        if (
+            typeof data.name === 'string' &&
+            !Object.values(Products).includes(data.name)
+        ) {
+            throw new Error(`product name not avaliable`);
+        }
+        const product = await create_product(data);
+        if (!product?.success) {
+            throw new Error(`${product?.message}`);
+        }
+        return {
+            success: true,
+            data: product?.data,
+        };
     } catch (error: any) {
         return {
             success: false,
@@ -53,26 +41,18 @@ const product_create = async (data: product_field_create) => {
 
 const product_search = async (data: product_field_search) => {
     try {
-        const valid = await valid_search_product(data);
-        if (!valid.error) {
-            const products = await search_product(data);
-            if (products?.success) {
-                return {
-                    success: true,
-                    data: products?.data,
-                };
-            } else {
-                return {
-                    success: false,
-                    message: products?.message,
-                };
-            }
-        } else {
-            return {
-                success: false,
-                message: valid.error.message,
-            };
+        const valid = valid_search_product(data);
+        if (valid.error) {
+            throw new Error(`${valid?.error.message}`);
         }
+        const products = await search_product(data);
+        if (!products?.success) {
+            throw new Error(`${products?.message}`);
+        }
+        return {
+            success: true,
+            data: products?.data,
+        };
     } catch (error: any) {
         return {
             success: false,
