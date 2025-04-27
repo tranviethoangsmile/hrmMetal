8aa7960fc6a6d6e99ba138ab0be0b2315da8e679
hoangdev <hoangdev@hoangdevs-iMac.local>
2025-04-27 22:18:50 +0900

    fix information search

--- src/validates/infomation/infomation.validate.ts
+++ src/validates/infomation/infomation.validate.ts
@@ -22,7 +22,6 @@ const schema_search_all_information_validate = Joi.object({
     content: Joi.string(),
     date: Joi.string()
         .pattern(/^\d{4}-\d{2}-\d{2}$/)
-        .required()
         .messages({
             'string.pattern.base': 'Date must be in the format yyyy-mm-dd',
         }),
