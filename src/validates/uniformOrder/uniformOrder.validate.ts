45998d195d2c65d950b7653cd366d4c61822ff1b
hoangdev <hoangdev@hoangdevs-iMac.local>
2025-04-27 23:03:32 +0900

    update

--- src/validates/uniformOrder/uniformOrder.validate.ts
+++ src/validates/uniformOrder/uniformOrder.validate.ts
@@ -12,7 +12,6 @@ const schema_create_uniform_order = Joi.object({
     order_status: Joi.string(),
     delivery_date: Joi.string()
         .pattern(/^\d{4}-\d{2}-\d{2}$/)
-        .required()
         .messages({
             'string.pattern.base': 'Date must be in the format yyyy-mm-dd',
         }),
