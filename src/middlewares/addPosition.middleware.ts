96c48ab742b1ec96d29e32cd8c171654f8d08350
hoangdev <hoangdev@hoangdevs-MacBook-Pro.local>
2025-04-30 16:30:15 +0900

    update

--- src/middlewares/addPosition.middleware.ts
+++ src/middlewares/addPosition.middleware.ts
@@ -1,24 +1,20 @@
 import { Request, Response, NextFunction } from 'express';
-import { findUserById } from '../useCases/user/user.useCase';
+import { findUserById } from '../useCases';
 
 const addPosition = async (req: Request, res: Response, next: NextFunction) => {
-    const { user_id } = req.body;
-
-    if (typeof user_id !== 'string' || !user_id.trim()) {
-        return res.status(400).json({
-            success: false,
-            message: 'Invalid User ID',
-        });
-    }
-
     try {
+        const { user_id } = req.body;
+        if (typeof user_id !== 'string' || !user_id.trim()) {
+            return res.status(400).json({
+                success: false,
+                message: 'Invalid User ID',
+            });
+        }
         const user = await findUserById(user_id);
-
         if (user?.success) {
             req.body.position = user.data?.position;
             return next();
         }
-
         return res.status(200).json({
             success: false,
             message: user?.message || 'Failed to retrieve user data',
