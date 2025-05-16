effccd2f48afad9020b8b32f3c0a2e4bf6dac3da
hoangdev <hoangdev@hoangdevs-iMac.local>
2025-05-16 18:09:06 +0900

    FB[firebase]: modify fb

--- src/utils/pushNotification/handlePushNotification.ts
+++ src/utils/pushNotification/handlePushNotification.ts
@@ -4,23 +4,24 @@ import dotenv from 'dotenv';
 import { IServiceAccount } from '../../interfaces';
 dotenv.config();
 const ENV = process.env;
-const serviceAccount = ENV.FIREBASE_CONFIG
-    ? JSON.parse(ENV.FIREBASE_CONFIG)
-    : {
-          type: ENV.FB_TYPE,
-          project_id: ENV.FB_PROJECT_ID,
-          private_key_id: ENV.FB_PRIVATE_KEY_ID,
-          private_key: ENV.FB_PRIVATE_KEY,
-          client_email: ENV.FB_CLIENT_EMAIL,
-          client_id: ENV.FB_CLIENT_ID,
-          auth_uri: ENV.FB_AUTH_URI,
-          token_uri: ENV.FB_TOKEN_URI,
-          auth_provider_x509_cert_url: ENV.FB_AUTH_PROVIDER_X509_CERT_URL,
-          client_x509_cert_url: ENV.FB_CLIENT_X509_CERT_URL,
-          universe_domain: ENV.FB_UNIVERSE_DOMAIN,
-      };
+const serviceAccount: IServiceAccount = {
+    type: ENV.FB_TYPE || 'service_account',
+    project_id: ENV.FB_PROJECT_ID || 'project_id',
+    private_key_id: ENV.FB_PRIVATE_KEY_ID || 'private_key_id',
+    private_key: ENV.FB_PRIVATE_KEY || 'private_key',
+    client_email: ENV.FB_CLIENT_EMAIL || 'client_email',
+    client_id: ENV.FB_CLIENT_ID || 'client_id',
+    auth_uri: ENV.FB_AUTH_URI || 'auth_uri',
+    token_uri: ENV.FB_TOKEN_URI || 'token_uri',
+    auth_provider_x509_cert_url:
+        ENV.FB_AUTH_PROVIDER_X509_CERT_URL || 'auth_provider_x509_cert_url',
+    client_x509_cert_url: ENV.FB_CLIENT_X509_CERT_URL || 'client_x509_cert_url',
+    universe_domain: ENV.FB_UNIVERSE_DOMAIN || 'universe_domain',
+};
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert(
+        JSON.parse(JSON.stringify(serviceAccount)),
+    ),
 });
 
 const sendPushNotification = async ({
