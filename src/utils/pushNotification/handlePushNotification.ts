980ff49aa93f08f866289c63daec1833a0e3d13b
hoangdev <hoangdev@hoangdevs-iMac.local>
2025-05-17 09:43:27 +0900

    FB[firebase]: modify fb

--- src/utils/pushNotification/handlePushNotification.ts
+++ src/utils/pushNotification/handlePushNotification.ts
@@ -1,14 +1,24 @@
-import { required } from '@hapi/joi';
 import admin from 'firebase-admin';
 import dotenv from 'dotenv';
-import { IServiceAccount } from '../../interfaces';
 dotenv.config();
 const ENV = process.env;
-const serviceAccount = ENV.FIREBASE_CONFIG;
+const serviceAccount = ENV.FIREBASE_CONFIG
+    ? JSON.parse(ENV.FIREBASE_CONFIG)
+    : {
+          type: 'service_account',
+          project_id: ENV.FIREBASE_PROJECT_ID,
+          private_key_id: ENV.FIREBASE_PRIVATE_KEY_ID,
+          private_key: ENV.FB_PRIVATE_KEY,
+          client_email: ENV.FIREBASE_CLIENT_EMAIL,
+          client_id: ENV.FIREBASE_CLIENT_ID,
+          auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+          token_uri: 'https://oauth2.googleapis.com/token',
+          auth_provider_x509_cert_url:
+              'https://www.googleapis.com/oauth2/v1/certs',
+          client_x509_cert_url: ENV.FIREBASE_CLIENT_CERT_URL,
+      };
 admin.initializeApp({
-    credential: admin.credential.cert(
-        JSON.parse(JSON.stringify(serviceAccount)),
-    ),
+    credential: admin.credential.cert(serviceAccount),
 });
 
 const sendPushNotification = async ({
