f4175e84dcfc7a5abebf849071556ac5a2e5cd39
hoangdev <hoangdev@hoangdevs-iMac.local>
2025-05-18 19:06:52 +0900

    cicd[update]: update file cicd

--- src/utils/pushNotification/handlePushNotification.ts
+++ src/utils/pushNotification/handlePushNotification.ts
@@ -2,21 +2,19 @@ import admin, { ServiceAccount } from 'firebase-admin';
 import dotenv from 'dotenv';
 dotenv.config();
 const ENV = process.env;
-const serviceAccount: ServiceAccount = ENV.FIREBASE_CONFIG
-    ? JSON.parse(ENV.FIREBASE_CONFIG)
-    : {
-          type: ENV.FB_TYPE,
-          project_id: ENV.FB_PROJECT_ID,
-          private_key_id: ENV.FB_PRIVATE_KEY_ID,
-          private_key: ENV.FB_PRIVATE_KEY,
-          client_email: ENV.FB_CLIENT_EMAIL,
-          client_id: ENV.FB_CLIENT_ID,
-          auth_uri: ENV.FB_AUTH_URI,
-          token_uri: ENV.FB_TOKEN_URI,
-          auth_provider_x509_cert_url: ENV.FB_AUTH_PROVIDER_X509_CERT_URL,
-          client_x509_cert_url: ENV.FB_CLIENT_X509_CERT_URL,
-          universe_domain: ENV.FB_UNIVERSE_DOMAIN,
-      };
+const serviceAccount: ServiceAccount = {
+    // type: ENV.FB_TYPE,
+    projectId: ENV.FB_PROJECT_ID,
+    // private_key_id: ENV.FB_PRIVATE_KEY_ID,
+    privateKey: ENV.FB_PRIVATE_KEY,
+    clientEmail: ENV.FB_CLIENT_EMAIL,
+    // client_id: ENV.FB_CLIENT_ID,
+    // auth_uri: ENV.FB_AUTH_URI,
+    // token_uri: ENV.FB_TOKEN_URI,
+    // auth_provider_x509_cert_url: ENV.FB_AUTH_PROVIDER_X509_CERT_URL,
+    // client_x509_cert_url: ENV.FB_CLIENT_X509_CERT_URL,
+    // universe_domain: ENV.FB_UNIVERSE_DOMAIN,
+};
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
 });
