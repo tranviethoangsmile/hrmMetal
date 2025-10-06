d85b1081202b13aaf00f8c2705565b601d9c4b69
hoangdev <hoangdev@hoangdevs-Mac-mini.local>
2025-10-06 12:24:51 +0900

    update[ratelimit]: up time

--- src/middlewares/rateLimit.config.ts
+++ src/middlewares/rateLimit.config.ts
@@ -2,13 +2,14 @@ import { Request } from 'express';
 import { rateLimit } from 'express-rate-limit';
 
 export const apiRateLimiter = rateLimit({
-    windowMs: 1 * 60 * 1000, 
-    max: 10,
+    windowMs: 1 * 60 * 1000,
+    max: 100,
     message: {
         success: false,
-        message: 'Too many requests from this IP, please try again after a minute',
+        message:
+            'Too many requests from this IP, please try again after a minute',
     },
-    legacyHeaders: false, 
+    legacyHeaders: false,
     skipSuccessfulRequests: false,
     skipFailedRequests: false,
 });
