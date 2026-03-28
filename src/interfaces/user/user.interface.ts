24bde2dc9a005a194eff79e302bea4851deea049
hoangdev <hoangdev@hoangdevnoMacBook-Pro.local>
2026-03-28 18:32:50 +0900

    cleanCode[create user]: check request

--- src/interfaces/user/user.interface.ts
+++ src/interfaces/user/user.interface.ts
@@ -39,17 +39,16 @@ interface CreateField {
     employee_id: number;
     is_active?: boolean;
     is_admin?: boolean;
-    is_officer?: boolean;
-    role?: string;
-    position?: string;
+    is_officer: boolean;
+    role: string;
+    position: string;
     department_id: string;
-    begin_date?: string;
-    is_offical_staff?: boolean;
-    salary_hourly?: number;
-    shift_night_pay?: number;
-    travel_allowance_pay?: number;
-    paid_days?: number;
-
+    begin_date: string;
+    is_offical_staff: boolean;
+    salary_hourly: number;
+    shift_night_pay: number;
+    travel_allowance_pay: number;
+    paid_days: number;
     department: Department;
 }
 
