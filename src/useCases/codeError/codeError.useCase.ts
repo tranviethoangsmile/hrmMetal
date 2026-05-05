edcc281abf5ba39d298e09612aaee72c9109d4a7
hoangdev <hoangdev@hoangdevnoMacBook-Pro.local>
2026-05-05 19:01:38 +0900

    handle[dailyReport]:add errors for daily report

--- src/useCases/codeError/codeError.useCase.ts
+++ src/useCases/codeError/codeError.useCase.ts
@@ -3,15 +3,15 @@ import {
     find_err_of_report,
 } from '../../repositorys/codeError/codeError.repository';
 import {
-    create_code_error,
-    search_code_error,
+    ICreate_code_error,
+    ISearch_code_error,
 } from '../../interfaces/codeError/codeError.interface';
 import { CodeError } from '../../enum/codeError.enum';
 import { valid_search_err } from '../../validates/codeError/codeError.validate';
 import { find_rp_by_id } from '../dailyReport/dailyReport.useCase';
 const create_err_for_report = async (data: any) => {
     try {
-        const errs: create_code_error[] = data;
+        const errs: ICreate_code_error[] = data;
         var check: boolean = false;
         var rp_id: string;
         for (let i = 0; i < errs.length; i++) {
@@ -57,7 +57,7 @@ const create_err_for_report = async (data: any) => {
     }
 };
 
-const find_error_of_report = async (data: search_code_error) => {
+const find_error_of_report = async (data: ISearch_code_error) => {
     try {
         const valid = await valid_search_err(data);
         if (!valid?.error) {
