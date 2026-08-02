69ce264a8088dcbe90edcdb7fe8564efe93fb0fd
hoangdev <hoangdev@hoangdevs-Mac-mini.local>
2026-08-02 22:01:23 +0900

    update

--- src/routers/information/infomationRouterModule/getInformationByField.router.ts
+++ src/routers/information/infomationRouterModule/getInformationByField.router.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import { GET_ALL_INFORMATION_WITH_FIELD_CONTROLLER } from '../../../controllers';
+
 const searchAllRouter: Router = Router();
 
-searchAllRouter.get('/', GET_ALL_INFORMATION_WITH_FIELD_CONTROLLER);
+searchAllRouter.post('/', GET_ALL_INFORMATION_WITH_FIELD_CONTROLLER);
+
 export default searchAllRouter;
